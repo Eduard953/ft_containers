2a7a98dc740325d271b85e0f5b0ab380da215cdc
ebeiline <ebeiline@students.42wolfsburg.de>
2022-06-14 21:43:14 +0300

    first constructors

--- vector.hpp
+++ vector.hpp
@@ -6,16 +6,19 @@
 /*   By: ebeiline <ebeiline@42wolfsburg.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 14:52:31 by ebeiline          #+#    #+#             */
-/*   Updated: 2022/06/14 16:00:58 by ebeiline         ###   ########.fr       */
+/*   Updated: 2022/06/14 21:43:01 by ebeiline         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef FT_VECTOR_HPP
+	#define FT_VECTOR_HPP
 
 #include <iostream>
 #include <stdexcept>
 
 namespace ft
 {
-	template < class T, class Alloc = allocator<T> > class vector
+	template < class T, class Alloc = std::allocator<T> > class vector
 	{
 		public:
 			typedef T												value_type;
@@ -30,7 +33,27 @@ namespace ft
 			// typedef ft::reverse_iterator<const_iterator>			const_reverse_iterator;
 			typedef std::ptrdiff_t									difference_type;
 			typedef std::size_t										size_type;
+
 		private:
-				
+			size_type _size;
+			size_type _capacity;
+			Alloc _alloc;
+
+		public:
+
+			explicit vector (const allocator_type& alloc = allocator_type())
+				: _size(0), _capacity(0)
+			{
+				this->_alloc = std::allocator<T>(alloc);
+			}
+			
+			explicit vector (size_type n, const value_type& val = value_type(),
+                 const allocator_type& alloc = allocator_type())
+				 : _size(n), _capacity(0)
+			{
+
+			}
 	}
 }
+
+#endif // FT_VECTOR_HPP
