78da1dbb0fd9f3deb0c7fb855ebbbf84f037bd4b
ebeiline <ebeiline@students.42wolfsburg.de>
2022-06-23 13:56:30 +0300

    basic structure iterator done

--- vec_iterator.hpp
+++ vec_iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: ebeiline <ebeiline@42wolfsburg.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 18:19:09 by ebeiline          #+#    #+#             */
-/*   Updated: 2022/06/21 18:29:48 by ebeiline         ###   ########.fr       */
+/*   Updated: 2022/06/23 13:55:55 by ebeiline         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,38 @@ namespace ft
 	class vector_iterator : public std::iterator<std::random_access_iterator_tag, T>
 	{
 		public:
-			typedef T							value_type;
-			typedef T*							pointer;
-			typedef T&							reference;
-			typedef std::ptrdiff_t				difference_type;
+			typedef	T												value_type;
+			typedef	std::size_t										size_type;
+			typedef long int										difference_type;
+
+			typedef typename choose<is_const, const T &, T &>::type	reference;
+			typedef typename choose<is_const, const T *, T *>::type	pointer;
+			typedef T*												elem_ptr;
 			
+		private:
+			elem_ptr												_val;
+
+		public:
+			// default constructors
+			vector_iterator(void): _val(NULL) {}
+			vector_iterator(elem_ptr val) : _val(val) {}
+
+			// copy constructors
+			vector_iterator(const vector_iterator<T, false>& other) {_val = other.getVal}
+			
+			// destructor
+			~vector_iterator() {}
+			
+			// assignment operator
+			vector_iterator& operator=(const vector_iterator& other)
+			{
+				if (this != &other)
+					_val = other._val;
+				return *this;
+			}
+
+			// getter
+			elem_ptr getVal(void) const {return _val;}
 			
 	}
 } // namespace ft
