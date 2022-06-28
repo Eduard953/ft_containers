45f7832b74058e7f21e6858bf1b7c9fe9764c338
ebeiline <ebeiline@students.42wolfsburg.de>
2022-06-28 14:50:07 +0300

    finished random_access_iterator

--- vec_iterator.hpp
+++ vec_iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: ebeiline <ebeiline@42wolfsburg.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 18:19:09 by ebeiline          #+#    #+#             */
-/*   Updated: 2022/06/23 13:55:55 by ebeiline         ###   ########.fr       */
+/*   Updated: 2022/06/28 14:48:58 by ebeiline         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 namespace ft
 {
-	template <typename T> 
+	template <typename T, bool B> 
 	class vector_iterator : public std::iterator<std::random_access_iterator_tag, T>
 	{
 		public:
@@ -51,6 +51,34 @@ namespace ft
 
 			// getter
 			elem_ptr getVal(void) const {return _val;}
+			
+			// Operator overloads
+			reference operator*(void) const {return *_val;}
+			pointer operator->(void) const {return _val;}
+			
+			vector_iterator& operator++() { _val++;	return *this;}
+			vector_iterator& operator--() { _val--;	return *this;}
+			vector_iterator operator++(int) { vector_iterator tmp(*this); ++(*this); return tmp;}
+			vector_iterator operator--(int) { vector_iterator tmp(*this); --(*this); return tmp;}
+
+			bool operator<(const vector_iterator& other) const {return _val < other._val;}
+			bool operator>(const vector_iterator& other) const {return _val > other._val;}
+			bool operator<=(const vector_iterator& other) const {return _val <= other._val;}
+			bool operator>=(const vector_iterator& other) const {return _val >= other._val;}
+			bool operator==(const vector_iterator& other) const {return _val == other._val;}
+			bool operator!=(const vector_iterator& other) const {return _val != other._val;}
+
+			difference_type operator-(const vector_iterator& other) const {return _val - other._val;}
+			
+			vector_iterator operator-(const int n) const {return vector_iterator(_val - n);}
+			vector_iterator operator+(const int n) const {return vector_iterator(_val + n);}
+
+			vector_iterator& operator-=(const int n) {_val -= n; return *this;}
+			vector_iterator& operator+=(const int n) {_val += n; return *this;}
+
+			reference operator[](const int n) const {return _val[n];}
+			
+			friend vector_iterator operator+(const int n, vector_iterator other) {return vector_iterator(other._val + n);}
 			
 	}
 } // namespace ft
